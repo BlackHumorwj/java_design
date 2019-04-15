1e9f5aeb10d8c910b067bdd0fc1556973371cee2
jinbao <jinbao@cash360.cn>
2019-04-15 20:29:21 +0800

    1、自定义View学习

---
+++ app/src/main/java/cn/cash360/ui/activity/sidazujian/broadcastreceiver/MyBroadcastReceiver.java
@@ -0,0 +1,19 @@
+package cn.cash360.ui.activity.sidazujian.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @time 2019/4/15 20:02
+ * @desc
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
