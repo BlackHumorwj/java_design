0cfbb9b6e3e79d1c24932679aebaf3b040067f64
jinbao <jinbao@cash360.cn>
2019-09-02 10:06:48 +0800

    提交 Thread

---
+++ app/src/main/java/cn/cash360/ui/activity/java/ThreadActivity.java
@@ -0,0 +1,123 @@
+package cn.cash360.ui.activity.java;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.util.Log;
+
+import cn.cash360.java_design.R;
+import cn.cash360.ui.activity.base.BaseActivity;
+
+/**
+ * @time 2019/8/30 11:54
+ * @desc
+ */
+public class ThreadActivity extends BaseActivity {
+
+
+    public static Intent newInstance(Context context) {
+        Intent intent = new Intent(context, ThreadActivity.class);
+        return intent;
+    }
+
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_thread;
+    }
+
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+
+        final User user = User.getInstance();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                user.doSth("thread");
+            }
+        });
+        thread.start();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                user.doSth("thread1");
+            }
+        });
+        thread1.start();
+
+    }
+
+
+    public static class User {
+
+        private static User mUser;
+        private boolean isOperate;
+
+        public static User getInstance() {
+            if (mUser == null) {
+                mUser = new User();
+            }
+            return mUser;
+        }
+
+
+        public void doSth(String url) {
+
+
+            Log.e("xx", SystemClock.currentThreadTimeMillis() + "--" + url);
+
+            if (isOperate) {
+                try {
+                    User.this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.e("xx", "进来了" + "--" + url);
+            }
+            synchronized (User.class) {
+
+                isOperate = true;
+
+
+                //            new Thread(new Runnable() {
+                //
+                //                @Override
+                //                public void run() {
+                //
+                //                    SystemClock.sleep(2000);
+                //
+                //
+                //                    Log.e("xx", "ggg");
+                //
+                //                    try {
+                //                        User.this.notifyAll();
+                //                    } catch (Exception e) {
+                //                        e.printStackTrace();
+                //                    }
+                //
+                //
+                //                }
+                //            }).start();
+
+                SystemClock.sleep(2000);
+
+
+                Log.e("xx", "ggg");
+
+                try {
+                    User.this.notifyAll();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
+
+    }
+}
--- app/src/main/java/cn/cash360/ui/fragment/tab/JavaFragment.java
+++ app/src/main/java/cn/cash360/ui/fragment/tab/JavaFragment.java
@@ -1,15 +1,17 @@
 package cn.cash360.ui.fragment.tab;
 
 import android.os.Bundle;
+import android.view.View;
 
 import cn.cash360.java_design.R;
+import cn.cash360.ui.activity.java.ThreadActivity;
 
 /**
  * @time 2019/4/12 12:53
  * @desc
  */
 
-public class JavaFragment extends BaseFragment {
+public class JavaFragment extends BaseFragment implements View.OnClickListener {
 
     public static JavaFragment newInstance() {
         Bundle args = new Bundle();
@@ -20,6 +22,20 @@ public class JavaFragment extends BaseFragment {
 
     @Override
     public int getLayoutId() {
-        return R.layout.fragment_primer_layout;
+        return R.layout.fragment_java_layout;
+    }
+
+    @Override
+    protected void initView(View view) {
+        super.initView(view);
+        view.findViewById(R.id.tv_thread).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int vId = v.getId();
+        if (vId == R.id.tv_thread) {
+            startActivity(ThreadActivity.newInstance(mActivity));
+        }
     }
 }
